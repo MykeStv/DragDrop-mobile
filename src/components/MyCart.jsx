d1f050e1dc8de78cf53bb23a0b32049f7ea67a7a
MykeStv <mykestv22@gmail.com>
2022-04-06 23:29:56 -0500

    mycard and bootstrap

--- src/components/MyCart.jsx
+++ src/components/MyCart.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const MyCart = () => {
     return (
-        <div>MyCart</div>
+        <div>
+            <div className='cards'>
+                <div className="card centered">
+                    <img
+                        className='card-img-top'
+                        src="/images/phone.jpg"
+                    />
+                    <div className="card-body">
+
+                        <h5 className='card-title'>Test_Phone</h5>
+                        <p className='card-subtitle'>$ 80</p>
+                        <p className='card-text'>12 minutes</p>
+                        <p className='card-text'>
+                            8G RAM, 16G memory
+                        </p>
+
+                        <div className='buttons'>
+                            <button className='btn btn-success'>Buy</button>
+                            <button className='btn btn-danger'>Decline</button>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
     )
 }
 
